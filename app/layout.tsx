2866d2f30ff60e32711c850a1f4e75077665f851
webdevabdul0 <ah0918047@gmail.com>
2025-02-14 15:52:25 +0500

    Added Google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
+import Script from "next/script";
+
+const GTAG_ID = "G-B2DEL0NCC1"; // Your Google Tag ID
 
 import "./globals.css";
 
@@ -23,6 +26,21 @@ export default function RootLayout({
       lang="en"
       className="dark bg-background text-primaryText overflow-x-hidden"
     >
+      <head>
+        {/* Google Tag Script */}
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GTAG_ID}`}
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GTAG_ID}');
+          `}
+        </Script>
+      </head>
       <body className={`${manrope.variable} antialiased`}>{children}</body>
     </html>
   );
